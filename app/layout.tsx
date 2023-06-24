c5a0107cfb34c2170d41f44a8b1dfc7b65e7d7dd
hamza <hk48709@gmail.com>
2023-06-24 13:30:48 +0500

    footer added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Nav from './components/Nav'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
